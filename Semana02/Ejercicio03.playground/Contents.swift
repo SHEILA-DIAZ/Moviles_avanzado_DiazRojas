27f6529e6d6f901d8b631b59c5a9af7cdd915126
SHEILA-DIAZ <sheila.diaz@tecsup.edu.pe>
2026-08-26 10:08:19 -0500

    "feat: validar pagos"

--- Semana02/Ejercicio03.playground/Contents.swift
+++ Semana02/Ejercicio03.playground/Contents.swift
@@ -1,3 +1,50 @@
-import UIKit
+import Foundation
 
-var greeting = "Hello, playground"
+// 1. Lectura de datos básicos
+print("Nombre Producto:")
+let producto = readLine() ?? ""
+
+print("Precio Unitario:")
+let precioUnitario = Double(readLine() ?? "0") ?? 0.0
+
+print("Cantidad:")
+let cantidad = Int(readLine() ?? "0") ?? 0
+
+print("Plan de Pago (6, 12, 24):")
+let mesesPlan = Int(readLine() ?? "0") ?? 12
+
+// Validar plan de pago
+var porcentajeInteres = 0.0
+if mesesPlan == 6 {
+    porcentajeInteres = 0.20
+} else if mesesPlan == 12 {
+    porcentajeInteres = 0.40
+} else if mesesPlan == 24 {
+    porcentajeInteres = 0.60
+} else {
+    print("Plan no válido.")
+}
+
+// 2. Lectura de pago adelantado
+print("¿En qué mes hará un pago Adelantado? (0 = ninguno):")
+let mesAdelantado = Int(readLine() ?? "0") ?? 0
+
+print("Monto Adicional a Pagar ese Mes (S/.):")
+let montoAdicional = Double(readLine() ?? "0") ?? 0.0
+
+// 3. Cálculos iniciales
+let montoCompra = precioUnitario * Double(cantidad)
+let interes = montoCompra * porcentajeInteres
+let montoFinanciado = montoCompra + interes
+let cuotaMensual = montoFinanciado / Double(mesesPlan)
+
+print("\n===== PLAN DE PAGO =====")
+print("Mes \t Fecha \t\t Monto Inicial \t Pago \t\t Resta por Pagar")
+
+var saldo = montoFinanciado
+var calendario = Calendar.current
+var fechaActual = Date() // Toma la fecha de hoy
+let dateFormatter = DateFormatter()
+dateFormatter.dateFormat = "dd/MM/yyyy"
+
+var mesesEfectivamentePagados = 0
